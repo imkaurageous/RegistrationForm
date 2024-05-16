34a66a911ba07489153c7e2af881921899a86989
beingkaurageous <erjkdhillon@gmail.com>
2024-05-16 11:10:23 +0530

    Registeration and Login Form

---
+++ RegistrationForm/Controllers/UsersController.cs
@@ -0,0 +1,82 @@
+﻿using RegistrationForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationForm.Controllers
+{
+    public class UsersController : Controller
+    {
+        private DataContext db = new DataContext();
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+            //var user = db.Users.ToList();
+            //return View(user);
+
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                ModelState.AddModelError("", "Some Error Occured!");
+            }
+            return View(user);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Login(Users user)
+        {
+            if(ModelState.IsValid)
+            {
+                using (DataContext db = new DataContext())
+                {
+                    var obj = db.Users.Where(u => u.Username.Equals(user.Username) && u.Password.Equals(user.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserId"] = obj.UserId.ToString();
+                        Session["Username"] = obj.Username.ToString();
+                        return RedirectToAction("LoggedIn");
+                    }
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if(Session["UserId"] !=null)
+            {
+                return View();
+            }
+            else
+            {
+               return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ RegistrationForm/Models/UserModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Email is Required")]
+        [Display(Name = "Email ID")]
+        [DataType(DataType.EmailAddress, ErrorMessage ="Email is Not Valid")]
+        public string Email { get; set; }
+
+        [Display(Name = "Mobile")]
+        public string Mobile { get; set; }
+    }
+}
